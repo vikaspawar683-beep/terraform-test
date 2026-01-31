c18ad86ca4ff049d2fd6a83cff781d1c7a325f34
vikaspawar683-beep <vikaspawar683@gmail.com>
2026-01-31 22:57:21 +0530

    Add security groups for ALB and EC2 instances
    Added security groups for ALB and EC2 instances with ingress and egress rules.

--- main.tf
+++ main.tf
@@ -1,7 +1,45 @@
-resourse"aws_instance""app_server"{
-ami      ="ami-019715e0d74f695be"
-instance_type="t3.micro"
-tags     ={
-name     ="learn-terraform"
+resource "aws_security_group" "alb_sg" {
+  name = "alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+resource "aws_security_group" "ec2_sg" {
+  name = "ec2-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
