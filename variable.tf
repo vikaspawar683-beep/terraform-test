5d6190ffbf8d6389d07484254953ec20fca621ff
vikaspawar683-beep <vikaspawar683@gmail.com>
2026-01-31 23:02:58 +0530

    Add AWS region, VPC ID, and subnet variables
    Added new variables for AWS region, VPC ID, and subnet IDs.

--- variable.tf
+++ variable.tf
@@ -1,11 +1,21 @@
-variable"instance_type"{
-  type  =string
-  description="EC2 instance type for the web server"
-  default  ="t2.micro"
+variable "aws_region" {
+  default = "ap-south-1"
 }
 
-variable"ami"{
-  type  =string
-  description="ami-019715e0d74f695be" #copy your instance ami id
+variable "vpc_id" {
+  description = "Your VPC ID"
 }
-  
+
+variable "subnet_ids" {
+  type = list(string)
+  description = "Public subnet IDs for ALB"
+}
+
+variable "ami_id" {
+  default = "ami-0f5ee92e2d63afc18" # Amazon Linux (update if needed)
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
