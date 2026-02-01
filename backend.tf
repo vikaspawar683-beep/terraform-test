daa0cd36c56ec1e48cfba491f1dc525386285a44
vikaspawar683-beep <vikaspawar683@gmail.com>
2026-02-01 12:43:35 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-12345"
-    key            = "alb-project/terraform.tfstate"
+    bucket         = "s3-bucket-beckend"
+    key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
